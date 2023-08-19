141557c4422444e71cdfea8561859bd715405e94
Stephon <stidd@mail.valenciacollege.edu>
2023-08-18 23:19:53 -0400

    commit for error pages on new branch

--- hello.py
+++ hello.py
@@ -15,6 +15,20 @@ def user(name):
     pizza = ["Cheese", "Mushroom", "Pineapple", "chicken"]
     return render_template("user.html", user_name=name, pizza = pizza)
 
+#create custom error page
+
+#invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
+
 
 
 if __name__ == '__main__':
