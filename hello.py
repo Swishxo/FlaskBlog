4c5b0a49e8dfc3ea8c86c19c9a48b67e1d41ba31
Stephon Tidd <stidd@mail.valenciacollege.edu>
2023-08-20 11:01:19 -0400

    checking if form worked

--- hello.py
+++ hello.py
@@ -6,17 +6,14 @@ from wtforms.validators import DataRequired
 #our app instance
 app = Flask(__name__)
 
-# Create a secret key for form. Purpose to ensure for cant get hacked
+# Create a secret key for form. Purpose to ensure it cant get hacked
 app.config['SECRET_KEY'] = 'Password' # can be any length
 
-#create a form class
+#create a form class. Class will be used inconjuction with our functions
 class NamerForm(FlaskForm):
     #define what you want to use in your form
     name = SearchField("Enter your Name", validators=[DataRequired()]) #'validators' check if form is filled in w/ 'DataRequired()'
     submit = SubmitField("Submit")
-
-
-
 
 
 #create route aka URL
@@ -28,7 +25,7 @@ def index():
 def user(name):
     #passing list to index "other Datastructure work the same way"
     pizza = ["Cheese", "Mushroom", "Pineapple", "chicken"]
-    return render_template("user.html", user_name=name, pizza = pizza)
+    return render_template("user.html", user_name = name, pizza = pizza)
 
 
 @app.route("/base")
@@ -45,9 +42,18 @@ def page_not_found(e):
 #internal server error
 @app.errorhandler(500)
 def page_not_found(e):#passing in e "for ERROR"
-    return render_template("500.html"), 500 #corresponding erros for functions
+    return render_template("500.html"), 500 #corresponding error (e) for functions
 
-
+#Create a page for form
+app.route("/name", methods=['GET', 'POST'])
+def name():
+    name = None #we use 'None' because no value is provided when form first loads
+    form = NamerForm() #the form class created above
+    # validating form
+    if form.validate_on_submit(): #check if form is filled when submit button is click
+        name = form.name.data #get class 'NamerForm' variable (name) data
+        form.name.data = '' #replace data w/ empty string
+    return render_template("name.html", name = name, form = form) #passing the data to HTML file
 
 
 if __name__ == '__main__':
