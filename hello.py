baf5b1654fefffeecc3d56d0374338bd2995ad77
Stephon Tidd <stidd@mail.valenciacollege.edu>
2023-08-20 10:24:56 -0400

    First commit for forms branch

--- hello.py
+++ hello.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import SearchField, SubmitField
+from wtforms.validators import DataRequired
 
 #our app instance
 app = Flask(__name__)
+
+# Create a secret key for form. Purpose to ensure for cant get hacked
+app.config['SECRET_KEY'] = 'Password' # can be any length
+
+#create a form class
+class NamerForm(FlaskForm):
+    #define what you want to use in your form
+    name = SearchField("Enter your Name", validators=[DataRequired()]) #'validators' check if form is filled in w/ 'DataRequired()'
+    submit = SubmitField("Submit")
+
+
+
 
 
 #create route aka URL
@@ -31,10 +46,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):#passing in e "for ERROR"
     return render_template("500.html"), 500 #corresponding erros for functions
-
-
-
-
 
 
 
